59ec0308761f4cf16b176b4a25a098457213c4ce
Hristo Roque <aroquega@gmail.com>
2025-09-03 20:51:41 -0500

    fix problems with path base

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://aroquega-codes.github.io',
+    base: '/faq-accordion',
     vite: {
         plugins: [tailwindcss()]
     }
